61a6c9c990c30241056883f7c5762d51ab301166
starver20 <amarnarute2002@gmail.com>
2022-05-10 17:28:37 +0530

    Chore: remove unused variables

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import classes from './Question.module.css';
-import { Link } from 'react-router-dom';
 import Option from '../option/Option';
 
 const Question = () => {
